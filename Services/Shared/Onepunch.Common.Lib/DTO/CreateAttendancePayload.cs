bb8145a49d63475ca32f2f5ec4bf4dd62b4dca6a
jhoncee <junnelhiro@hiro-c.net>
2026-05-17 14:30:24 +0800

    add IP

--- Services/Shared/Onepunch.Common.Lib/DTO/CreateAttendancePayload.cs
+++ Services/Shared/Onepunch.Common.Lib/DTO/CreateAttendancePayload.cs
@@ -18,9 +18,9 @@ public class CreateAttendancePayload
     public Guid? ClientId { get; set; }
     public Guid? DepartmentId { get; set; }
     public string DeviceName { get; set; } = string.Empty;
+    public string IPAddress  { get; set; } = string.Empty;
     public Polygon? Coordinates { get; set; }
 }
-
 
 public record BatchAttConfirmation
 {
