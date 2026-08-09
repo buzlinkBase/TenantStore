e00836b6d7befd6296257a2a2293732655042ede
jhoncee <junnel@hiro-c.net>
2026-08-09 10:55:46 +0800

    update buzlinikrepo

--- Services/AuthApi/Onepunch.Auth.Domain/Entities/BaseEntity.cs
+++ Services/AuthApi/Onepunch.Auth.Domain/Entities/BaseEntity.cs
@@ -1,8 +1,14 @@
 ﻿
 using BuzlinkRepository;
+using Mapster;
 
 namespace OnePunch.Auth.Domain.Entities;
 
-public abstract class BaseEntity : EntityBase
+public abstract class BaseEntity : EntityBase, ITimeStamp
 {
+    [AdaptIgnore]
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    [AdaptIgnore]
+    public DateTime? DeletedAt { get; set; }
 }
