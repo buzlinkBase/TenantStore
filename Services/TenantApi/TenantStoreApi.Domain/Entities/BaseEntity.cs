e46b463f602d545320b48d94456f4babc38b67aa
jhoncee <junnel@hiro-c.net>
2026-08-11 22:00:07 +0800

    move created at

--- Services/TenantApi/TenantStoreApi.Domain/Entities/BaseEntity.cs
+++ Services/TenantApi/TenantStoreApi.Domain/Entities/BaseEntity.cs
@@ -1,7 +1,11 @@
 ﻿using BuzlinkRepository;
 namespace TenantStoreApi.Domain.Entities;
 
-public class BaseEntity : EntityBase
+public class BaseEntity : EntityBase, ITimeStamp
 {
-    public string Status { get; set; } = "Active";
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public string Status { get; set; } = "Active"; 
+
 }
